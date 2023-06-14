2447634c8135caf98b098a0e72310964839ca405
eiidiego <diegovotu2310@gmail.com>
2023-06-14 10:17:42 -0300

    up

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 let canvas = document.getElementById("gamesnake");
 let context = canvas.getContext("2d");
 let box = 32;
+let pontos = 0;
 
-// cobrinha
+// kobrinha
 let snake = [];
 
-// início cobrinha
-snake[0] ={
+// início kobrinha
+snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
@@ -16,22 +17,22 @@ snake[0] ={
 let direction = "right"
 
 // comida
-let food ={
+let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
 // fundo
 function criarBG() {
-    context.fillStyle = "lightgreen"
-// desenha retângulo usnado x e y
+    context.fillStyle = "green"
+    // desenha retângulo usnado x e y
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-// cria cobrinha
-function criaCobrinha (){
-    for(i = 0; i < snake.length; i++) {
-        context.fillStyle = "green"
+// cria kobrinha
+function criaCobrinha() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "black"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
@@ -45,22 +46,23 @@ function drawFood() {
 // detecta e chama a função update, quando acontece evento
 document.addEventListener('keydown', update);
 
+
 function update(event) {
-    if (event.keyCode == 37 && direction != 'right') direction = 'left';
-    if (event.keyCode == 38 && direction != 'down') direction = 'up';
-    if (event.keyCode == 39 && direction != 'left') direction = 'right';
-    if (event.keyCode == 40 && direction != 'up') direction = 'down';
+    if (event.keyCode == (37 || 65) && direction != 'right') direction = 'left';
+    if (event.keyCode == (38 || 87) && direction != 'down') direction = 'up';
+    if (event.keyCode == (39 || 68) && direction != 'left') direction = 'right';
+    if (event.keyCode == (40 || 83) && direction != 'up') direction = 'down';
 }
 
 // função peincipal
 function iniciarJogo() {
-    if (snake[0].x > 15*box && direction == "right") {
+    if (snake[0].x > 15 * box && direction == "right") {
         snake[0].x = 0;
     }
     if (snake[0].x < 0 && direction == "left") {
         snake[0].x = 16 * box;
     }
-    if (snake[0].y > 15*box && direction == "down") {
+    if (snake[0].y > 15 * box && direction == "down") {
         snake[0].y = 0;
     }
     if (snake[0].y < 0 && direction == "up") {
@@ -68,12 +70,12 @@ function iniciarJogo() {
     }
 
     for (i = 1; i < snake.length; i++) {
-        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+        document.getElementById("pnts").innerText = pontos + " pontos"
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(jogo);
-            alert('Game Over >:O')
+            document.getElementById("pnts").innerText = "Após " + pontos + " pontos você perdeu";
         }
     }
-
     criarBG();
     criaCobrinha();
     drawFood();
@@ -81,25 +83,34 @@ function iniciarJogo() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if (direction == "right") snakeX += box; 
-    if (direction == "left") snakeX -= box; 
-    if (direction == "up") snakeY -= box; 
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
-    
+
     if (snakeX != food.x || snakeY != food.y) {
         // pop tira o último elemento da lista
-        snake.pop(); 
+        snake.pop();
     } else {
-        food.x = Math.floor(Math.random() * 15 +1) * box;
-        food.y = Math.floor(Math.random() * 15 +1) * box;
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+        pontos += 1;
     }
 
-    let newHead ={
+    let newHead = {
         x: snakeX,
         y: snakeY
     }
-// método unshift adiciona como primeiro quadrinho da cobrinha
+    // método unshift adiciona como primeiro quadrinho da cobrinha
     snake.unshift(newHead)
 }
 
 let jogo = setInterval(iniciarJogo, 100)
+
+function restart() {
+    resposta = confirm('Você deseja recomeçar?')
+    if (resposta) {
+        jogo = setInterval(0)
+    }
+
+}
